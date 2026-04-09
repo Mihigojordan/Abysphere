e020abc767dc568b8dea53c9194fbf3cd994facb
12345678+mihigojordan@users.noreply.github.com <12345678+mihigojordan@users.noreply.github.com>
2026-04-09 22:15:52 +0200

    fixed the env error

--- frontend/src/services/purchaseOrderService.ts
+++ frontend/src/services/purchaseOrderService.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { API_URL } from '../api/api';
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000';
+const BASE_URL =  API_URL || 'http://localhost:8000';
 
 export interface CreatePODto {
     supplierId: string;
